6b704a54825c33cdf8eae42f88d87187f54dfb2a
crice802 <rcrice@gmail.com>
2022-05-03 16:17:57 -0400

    push to github

--- src/main/java/com/test/project/HibernateJPABegginer/App.java
+++ src/main/java/com/test/project/HibernateJPABegginer/App.java
@@ -1,13 +1,18 @@
 package com.test.project.HibernateJPABegginer;
 
+import com.test.hib.controller.FindUser_Hql;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		FindUser_Hql u = new FindUser_Hql();
+		u.findUser();
+		u.findUserSelect();
+		u.getRecordbyId();
+		u.getmaxSalary();
+		u.NamedQueryExample();
+	}
 }
